f94a536f05424157cb7b074dc73d3d51de2c693f
mibrgmv <mibrgmv@gmail.com>
2025-03-07 01:27:13 +0300

    feat: auth progress

---
+++ auth.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"time"
+)
+
+var secretKey = []byte("your-secret-key")
+
+func getRole(username string) string {
+	if username == "root" {
+		return "root"
+	}
+	return "basic"
+}
+
+func createToken(username string) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": username,
+		"iss": "whoami",
+		"aud": getRole(username),
+		"exp": time.Now().Add(time.Hour).Unix(),
+		"iat": time.Now().Unix(),
+	})
+
+	tokenString, err := claims.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("Token claims added: %+v\n", claims)
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return token, nil
+}
+
+func authenticateMiddleware(c *gin.Context) {
+	tokenString, err := c.Cookie("token")
+	if err != nil {
+		fmt.Println("Token missing in cookie")
+		c.Redirect(http.StatusSeeOther, "/login")
+		c.Abort()
+		return
+	}
+
+	token, err := verifyToken(tokenString)
+	if err != nil {
+		fmt.Printf("Token verification failed: %v\\n", err)
+		c.Redirect(http.StatusSeeOther, "/login")
+		c.Abort()
+		return
+	}
+
+	fmt.Printf("Token verified successfully. Claims: %+v\\n", token.Claims)
+
+	c.Next()
+}
--- main.go
+++ main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+var loggedInUser string
+
 func main() {
 	gin.ForceConsoleColor()
 	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"LoggedIn": loggedInUser != "",
+			"Username": loggedInUser,
+			"Role":     getRole(loggedInUser),
+		})
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		if (username == "user" && password == "pass") || (username == "root" && password == "root") {
+			tokenString, err := createToken(username)
+			if err != nil {
+				c.String(http.StatusInternalServerError, "Error creating token")
+				return
+			}
+
+			loggedInUser = username
+			fmt.Printf("Token created: %s\n", tokenString)
+			c.SetCookie("token", tokenString, 3600, "/", "localhost", false, true)
+			c.Redirect(http.StatusSeeOther, "/")
+		} else {
+			c.String(http.StatusUnauthorized, "Invalid credentials")
+		}
+	})
+
+	router.GET("/logout", func(c *gin.Context) {
+		loggedInUser = ""
+		c.SetCookie("token", "", -1, "/", "localhost", false, true)
+		c.Redirect(http.StatusSeeOther, "/")
+	})
+
 	router.GET("/quiz", getQuizzes)
-	router.GET("/quiz/:id", getQuizByID)
+	router.GET("/quiz/:id", authenticateMiddleware, getQuizByID)
 
 	s := &http.Server{
 		Addr:    ":8080",
