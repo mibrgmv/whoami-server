1ff1693c93781e24b59f9340af330d8cc4a86982
mibrgmv <mibrgmv@gmail.com>
2025-03-07 13:19:31 +0300

    feat: postgres config

---
+++ configuration/config.go
@@ -0,0 +1,45 @@
+package configuration
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Config struct {
+	Postgres struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Database string `yaml:"database"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		SslMode  string `yaml:"ssl_mode"`
+	} `yaml:"postgres"`
+}
+
+func (c *Config) Load(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	config := &Config{}
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func (c *Config) GetPostgresConnectionString() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		c.Postgres.Username,
+		c.Postgres.Password,
+		c.Postgres.Host,
+		c.Postgres.Port,
+		c.Postgres.Database,
+		c.Postgres.SslMode,
+	)
+}
