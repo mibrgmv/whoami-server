62faf60e6ba3edf6e50aab203be78ec7ef23dcdd
mibrgmv <mibrgmv@gmail.com>
2025-03-11 14:05:30 +0300

    refac: config json fields

--- cmd/config.go
+++ cmd/config.go
@@ -8,13 +8,13 @@ import (
 
 type Config struct {
 	Postgres struct {
-		Host     string `yaml:"host"`
-		Port     int    `yaml:"port"`
-		Database string `yaml:"database"`
-		Username string `yaml:"username"`
-		Password string `yaml:"password"`
-		SslMode  string `yaml:"ssl_mode"`
-	} `yaml:"postgres"`
+		Host     string `yaml:"host" json:"host"`
+		Port     int    `yaml:"port" json:"port"`
+		Database string `yaml:"database" json:"database"`
+		Username string `yaml:"username" json:"username"`
+		Password string `yaml:"password" json:"password"`
+		SslMode  string `yaml:"ssl_mode" json:"ssl_mode"`
+	} `yaml:"postgres" json:"postgres"`
 }
 
 func (c *Config) Load(filename string) (*Config, error) {
