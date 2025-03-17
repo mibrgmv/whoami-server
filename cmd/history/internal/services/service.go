adb5eabd2a6d33588e5a6df99048a4167fe063b4
mibrgmv <mibrgmv@gmail.com>
2025-03-18 01:32:54 +0300

    landmark: history grpc service def

--- cmd/history/internal/services/service.go
+++ cmd/history/internal/services/service.go
@@ -2,21 +2,75 @@ package services
 
 import (
 	"context"
+	"google.golang.org/grpc"
 	"whoami-server/cmd/history/internal/models"
+	pb "whoami-server/protogen/golang/history"
 )
 
 type Service struct {
-	history Repository
+	repo Repository
+	pb.UnimplementedQuizCompletionHistoryServiceServer
 }
 
 func NewService(repo Repository) *Service {
-	return &Service{history: repo}
+	return &Service{repo: repo}
 }
 
-func (s *Service) Add(ctx context.Context, items []models.QuizCompletionHistoryItem) ([]models.QuizCompletionHistoryItem, error) {
-	return s.history.Add(ctx, items)
+func (s *Service) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
+	modelItems := make([]models.QuizCompletionHistoryItem, len(req.Items))
+	for i, item := range req.Items {
+		modelItems[i] = models.QuizCompletionHistoryItem{
+			ID:         item.Id,
+			QuizID:     item.QuizId,
+			UserID:     item.UserId,
+			QuizResult: item.QuizResult,
+		}
+	}
+
+	resultItems, err := s.repo.Add(ctx, modelItems)
+	if err != nil {
+		return nil, err
+	}
+
+	responseItems := make([]*pb.QuizCompletionHistoryItem, len(resultItems))
+	for i, item := range resultItems {
+		responseItems[i] = &pb.QuizCompletionHistoryItem{
+			Id:         item.ID,
+			QuizId:     item.QuizID,
+			UserId:     item.UserID,
+			QuizResult: item.QuizResult,
+		}
+	}
+
+	return &pb.AddResponse{Items: responseItems}, nil
 }
 
-func (s *Service) Query(ctx context.Context, query Query) ([]models.QuizCompletionHistoryItem, error) {
-	return s.history.Query(ctx, query)
+func (s *Service) Query(req *pb.QueryRequest, stream grpc.ServerStreamingServer[pb.QuizCompletionHistoryItem]) error {
+	ctx := stream.Context()
+
+	query := Query{
+		IDs:     req.Ids,
+		UserIDs: req.UserIds,
+		QuizIDs: req.QuizIds,
+	}
+
+	items, err := s.repo.Query(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range items {
+		protoItem := &pb.QuizCompletionHistoryItem{
+			Id:         item.ID,
+			QuizId:     item.QuizID,
+			UserId:     item.UserID,
+			QuizResult: item.QuizResult,
+		}
+
+		if err := stream.Send(protoItem); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
+	golang.org/x/crypto v0.36.0
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -47,11 +50,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.15.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	golang.org/x/tools v0.31.0 // indirect
-	google.golang.org/protobuf v1.36.5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 )
