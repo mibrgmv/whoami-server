bbc46d47f834cbfbd4f1b67a667ad7077b40a196
mibrgmv <mibrgmv@gmail.com>
2025-03-24 16:17:21 +0300

    agony: main for grpc + gateway

--- cmd/whoami/main.go
+++ cmd/whoami/main.go
@@ -1,22 +1,72 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
-	"os"
-	"whoami-server/cmd/whoami/internal/servers/grpc"
+	"net"
+	"net/http"
+	"whoami-server/cmd/whoami/internal/services/quiz"
+	pgquiz "whoami-server/cmd/whoami/internal/services/quiz/postgresql"
+	quizpb "whoami-server/protogen/golang/quiz"
 )
 
 func main() {
-	config, err := (&grpc.Config{}).Load("configs/default.yaml")
+	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalf("failed to load config: %v", err)
+		log.Fatalln("Failed to listen:", err)
 	}
 
-	server := grpc.NewServer(config)
-	fmt.Printf("Starting Whoami service on port %d\n", config.Server.Port)
-	if err := server.RunAndWait(); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-		os.Exit(1)
+	s := grpc.NewServer()
+
+	connString := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=prefer"
+	poolConfig, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		log.Fatalf("Unable to parse connStr: %v", err)
 	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v", err)
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(context.Background()); err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	log.Println("Connected to database successfully")
+
+	quizRepo := pgquiz.NewRepository(pool)
+	quizService := quiz.NewService(quizRepo)
+	quizpb.RegisterQuizServiceServer(s, quizService)
+
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	conn, err := grpc.NewClient(
+		"0.0.0.0:8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = quizpb.RegisterQuizServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
